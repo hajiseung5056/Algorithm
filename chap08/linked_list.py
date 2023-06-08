4ec54b78a791f6dd5d51fe5cbe8c5bce025bd67b
하지승 <cjswp051@naver.com>
2023-06-08 17:47:03 +0900

    1회차 - 12주차

--- chap08/linked_list.py
+++ chap08/linked_list.py
@@ -8,3 +8,15 @@ class Node:
     def __init__(self, data: Any= None,next: Node = None):
         self.data = data
         self.next = data
+
+
+class LinkedList:
+
+    def __init__(self) -> None:
+        self.no = 0
+        self.head = None
+        self.current = None
+
+    def __len__(self) -> int:
+
+        return self.no
