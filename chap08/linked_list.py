4a7782ee1561746f737c099c841cd58ad647f3fc
하지승 <cjswp051@naver.com>
2023-06-08 17:49:12 +0900

    1회차 - 12주차

--- chap08/linked_list.py
+++ chap08/linked_list.py
@@ -20,3 +20,18 @@ class LinkedList:
     def __len__(self) -> int:
 
         return self.no
+
+    def search(self, data: Any) -> int:
+        cnt = 0
+        ptr = self.head
+        while ptr is not None:
+            if ptr.data == data:
+                self.current = ptr
+                return cnt
+            cnt += 1
+            ptr = ptr.next
+        return -1
+
+    def __contains__(self, data: Any) -> bool:
+
+        return self.search(data) >= 0
