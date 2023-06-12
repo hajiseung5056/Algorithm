a6b489e3799fde58a0aa5bdf72d4753881d076de
하지승 <cjswp051@naver.com>
2023-06-12 12:14:09 +0900

    1회차 - 12주차

--- chap08/linked_list.py
+++ chap08/linked_list.py
@@ -102,3 +102,16 @@ class LinkedList:
             return False
         self.current = self.current.next
         return Ture
+
+    def print_current_node(self) -> None:
+        if self.current is None:
+            print('주목 노드가 존재하지 않습니다.')
+        else:
+            print(self.current.data)
+
+    def print(self) -> None:
+        ptr = self.head
+
+        while ptr is not None:
+            print(ptr.data)
+            ptr = ptr.next
