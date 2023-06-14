0f30f45ac08927d46ec5c3468056d3d6a7fdd1da
하지승 <cjswp051@naver.com>
2023-06-14 17:58:02 +0900

    1회차 - 13주차

---
+++ chap08/array_list.py
@@ -0,0 +1,55 @@
+# 커서로 연결 리스트 구현하기
+
+from __future__ import annotations
+from typing import Any, Type
+
+Null = -1
+
+class Node:
+
+    def __init__(self, data = Null, next = Null, dnext = Null):
+        self.data = data
+        self.next = next
+        self.dnext = dnext
+
+
+class ArrayLinkedList:
+    def __init__(self, capacity: int):
+        self.head = Null
+        self.current = Null
+        self.max = Null
+        self.deleted = Null
+        self.capacity = capacity
+        self.n = [Node()] * self.capacity
+        self.no = 0
+
+    def __len__(self) -> int:
+        return self.no
+
+    def get_insert_index(self):
+        if self.deleted == Null:
+            if self.max + 1 < self.capacity:
+                self.max += 1
+                return self.max
+            else:
+                return Null
+        else:
+            rec = self.deleted
+            self.deleted = self.n[rec].dnext
+            return rec
+
+    def delete_index(self, idx: int) -> None:
+        if self.deleted == Null:
+            delf.deleted = idx
+            self.n[idx].dnext = Null
+        else:
+            rec = self.deleted
+            self.deleted = idx
+            self.n[idx].dnext = rec
+
+        def search(self, data: Any) -> int:
+            cnt = 0
+            ptr = self.head
+            while ptr != Null:
+                if self.n[ptr].data == data:
+                    self.current = ptr
