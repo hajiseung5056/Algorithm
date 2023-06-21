8740ec1aec46d4a0ddc462912ca16416542f8b82
하지승 <cjswp051@naver.com>
2023-06-21 17:43:11 +0900

    1회차 - 13주차

--- chap08/double_list.py
+++ chap08/double_list.py
@@ -111,3 +111,41 @@ class DoubleLinkedList:
         while not self.is_empty():
             self.remove_first()
         self.no = 0
+
+    def __iter__(self) -> DoubleLinkedListIterator:
+        return DoubleLinkedListIterator(self.head)
+
+    def __reversed__(self) -> DoubleLinkedListReverseIterator:
+        return DoubleLinkedListReverseIterator(self.head)
+
+class DoubleLinkedListIterator:
+    def __init(self, head: Node):
+        self.head = head
+        self.current = head.next
+
+    def __iter__(self) -> DoubleLinkedListIterator:
+        return self
+
+    def __next__(self) -> Any:
+        if self.current is self.head:
+            raise StopIteration
+        else:
+            data = self.current.data
+            self.current = self.current.next
+            return data
+
+class DoubleLinkedListReverseIterator:
+    def __init(self, head: Node):
+        self.head = head
+        self.current = head.prev
+
+    def __iter__(self) -> DoubleLinkedListReverseIterator:
+        return self
+
+    def __next__(self) -> Any:
+        if self.current is self.head:
+            raise StopIteration
+        else:
+            data = self.current.data
+            self.current = self.current.prev
+            return data
