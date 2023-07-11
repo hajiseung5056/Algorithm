e19e7ff0901932e6fc85484d40300c3af6b7135c
하지승 <cjswp051@naver.com>
2023-07-11 12:32:20 +0900

    1회차 - 14주차

--- chap09/bst.py
+++ chap09/bst.py
@@ -1,1 +1,20 @@
 # 이진 검색 트리 구현하기
+
+from __future__ import annotations
+from typing import Any, Type
+
+class Node:
+
+    def __init__(self, key: Any, value:Any, left: Node = None, right: Node = None):
+        self.key = key
+        self.value = value
+        self.left = left
+        self.right = right
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    def search(self, key: Any):
+        p = self.root
