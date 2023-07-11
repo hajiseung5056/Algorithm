5ad911b0c0e563384c973b47311bb881a109c8f9
하지승 <cjswp051@naver.com>
2023-07-11 12:33:43 +0900

    1회차 - 14주차

--- chap09/bst.py
+++ chap09/bst.py
@@ -18,3 +18,14 @@ class BinarySearchTree:
 
     def search(self, key: Any):
         p = self.root
+        while True:
+            if p is None:
+                return None
+            if key == p.key:
+                return p.value
+            elif key < p.key:
+                p = p.left
+            else:
+                p = p.right
+
+
